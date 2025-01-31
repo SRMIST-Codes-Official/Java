a0fbef8fc326910cabc294b2a3af5e5859a883b9
Saket <saketruia18@gmail.com>
2025-02-01 03:06:41 +0530

    leetcode problem 367. Valid Perfect Square

---
+++ Leet Code/367. Valid Perfect Square/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        double ans = (double) Math.pow(num,0.5);
+        int abs = (int) ans;
+        if(ans%abs==0)
+        return true;
+        return false;
+    }
+}
