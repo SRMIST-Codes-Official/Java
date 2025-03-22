78315c1289287a8f2a3b48a30bdd7887c45d391f
Saket <saketruia18@gmail.com>
2025-03-22 05:41:59 +0530

    143 of leetcode

---
+++ Leet Code/143. Reorder List/LeetCode_Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null){
+            return;
+        }
+        ListNode f=head;
+        ListNode s=head;
+        while(f!= null && f.next!=null){
+            f=f.next.next;
+            s=s.next;
+        }
+        ListNode hs=reverse(s);
+        ListNode hf=head;
+        while(hf!=null && hs!=null){
+            ListNode temp=hf.next;
+            hf.next=hs;
+            hf=temp;
+            temp=hs.next;
+            hs.next=hf;
+            hs=temp;
+        }
+        if(hf!=null){
+            hf.next=null;
+        }
+    }
+    ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode next=null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
