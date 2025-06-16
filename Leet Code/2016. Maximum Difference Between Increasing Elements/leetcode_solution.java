68c7324b9d2e6061f03699a50f8f5855f6be14e6
Saket <saketruia18@gmail.com>
2025-06-16 17:01:42 +0530

    2016 of leetcode

---
+++ Leet Code/2016. Maximum Difference Between Increasing Elements/leetcode_solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int d=-1;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] <nums[j] && nums[j]-nums[i] >d){
+                    d=nums[j]-nums[i];
+                }
+            }
+        }
+        return d;
+    }
+}
