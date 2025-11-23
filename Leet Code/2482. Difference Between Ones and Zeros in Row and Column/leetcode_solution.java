cc1482c0bf24c0cd47446d2a09194f628ff4d2e6
Saket <saketruia18@gmail.com>
2025-11-23 18:00:00 +0530

    3746 of leetcode

---
+++ Leet Code/2482. Difference Between Ones and Zeros in Row and Column/leetcode_solution.java
@@ -0,0 +1,33 @@
+//13ms solution
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[][] diff=new int[grid.length][grid[0].length];
+        int[] rowsum=new int[grid.length];
+        int[] colsum=new int[grid[0].length];
+        for(int i=0;i<rowsum.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==0){
+                    rowsum[i]--;
+                }else{
+                    rowsum[i]++;
+                }
+            }
+        }
+        for(int i=0;i<colsum.length;i++){
+            for(int j=0;j<grid.length;j++){
+                if(grid[j][i]==0){
+                    colsum[i]--;
+                }else{
+                    colsum[i]++;
+                }
+            }
+        }
+        for(int i=0;i<diff.length;i++){
+            for(int j=0;j<diff[0].length;j++){
+                diff[i][j]=rowsum[i]+colsum[j];
+            }
+        }
+        return diff;
+    }
+    
+}
