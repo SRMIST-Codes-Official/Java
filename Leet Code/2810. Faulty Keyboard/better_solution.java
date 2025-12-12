1bc084966cc8ae1eb428c3c12b3ca50e94babf6f
Saket <saketruia18@gmail.com>
2025-12-12 19:07:32 +0530

    better solution of 2810

---
+++ Leet Code/2810. Faulty Keyboard/better_solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder st = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='i'){
+                st.append(s.charAt(i));
+            }else{
+                st.reverse();
+            }
+        }
+        return st.toString();
+    }
+}
