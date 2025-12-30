c5d58b5ed09f6300404dbb12ba7f7b60b8bc77b2
Saket <saketruia18@gmail.com>
2025-12-30 18:00:00 +0530

    3775 of leetcode

---
+++ Leet Code/3775. Reverse Words With Same Vowel Count/leetcode_solution.java
@@ -0,0 +1,33 @@
+//solution of 3775
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        int vowels=vc(words[0]);
+        String r=words[0];
+        for(int i=1;i<words.length;i++){
+            if(vc(words[i]) == vowels){
+                r=r+" "+rev(words[i]);
+            }else{
+                r=r+" "+words[i];
+            }
+        }
+        return r;
+
+    }
+    int vc(String s){
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if("aeiou".indexOf(s.charAt(i))!=-1){
+                c++;
+            }
+        }
+        return c;
+    }
+    String rev(String s){
+        String r="";
+        for(int i=0;i<s.length();i++){
+            r=s.charAt(i)+r;
+        }
+        return r;
+    }
+}
