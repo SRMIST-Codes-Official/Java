906601af26fbde27dabc0d88db59694cc8748beb
Saket <saketruia18@gmail.com>
2026-02-12 16:29:04 +0530

    better sollution of 41

---
+++ Leet Code/41. First Missing Positive/better_solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int[] arr= new int[nums.length+1];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0 && nums[i]<=nums.length){
+                arr[nums[i]]++;
+            }
+        }
+        int c=nums.length+1;
+        for(int i=1;i<nums.length+1;i++){
+            if(arr[i]==0){
+                c=i;
+                break;
+            }
+        }
+        return c;
+    }
+}
