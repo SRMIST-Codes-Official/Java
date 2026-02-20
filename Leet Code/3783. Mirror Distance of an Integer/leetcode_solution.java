fab49ca0ce63e8418c93a309b3e81e372cc0fff2
Saket <saketruia18@gmail.com>
2026-02-20 21:15:50 +0530

    3783 of leetcode

---
+++ Leet Code/3783. Mirror Distance of an Integer/leetcode_solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n-rev(n));
+    }
+    int rev(int n){
+        int r=0;
+        while(n>0){
+            int d=n%10;
+            r=(r*10)+d;
+            n=n/10;
+        }
+        return r;
+    }
+}
