f7accec3dd9d32fdd157878043815aba13cb82a1
Saket <saketruia18@gmail.com>
2026-03-24 21:31:06 +0530

    1886 of leetcode

---
+++ Leet Code/1886. Determine Whether Matrix Can Be Obtained By Rotation/leetcode_solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean findRotation(int[][] matrix, int[][] target) {
+        if (Arrays.deepEquals(matrix, target)) {
+            return true;
+        }
+        matrix=rt(matrix);
+        if (Arrays.deepEquals(matrix, target)) {
+            return true;
+        }
+        matrix=rt(matrix);
+        if (Arrays.deepEquals(matrix, target)) {
+            return true;
+        }
+        matrix=rt(matrix);
+        if (Arrays.deepEquals(matrix, target)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    int[][] rt(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return matrix;
+    }
+}
