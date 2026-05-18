7b1a215a76bd1ff4fafd0c0aad333f7c5a4ec111
Saket <saketruia18@gmail.com>
2026-05-18 23:22:47 +0530

    2744 of leetcode

---
+++ Leet Code/2744. Find Maximum Number of String Pairs/leetcode_solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int c=0;
+        for(int i=0;i<words.length;i++){
+            String rev=new StringBuilder(words[i]).reverse().toString();
+            for(int j=i+1;j<words.length;j++){
+                if(rev.equals(words[j])){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
