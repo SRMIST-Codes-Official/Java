81fe5e18981193cf32ac2355883aef53b126c876
Saket <saketruia18@gmail.com>
2026-06-24 14:20:02 +0530

    110 of leetcoode

---
+++ Leet Code/110. Balanced Binary Tree/leetcode_solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        int lh=depth(root.left);
+        int rh=depth(root.right);
+        if(Math.abs(lh-rh)>1){
+            return false;
+        }
+        boolean left=isBalanced(root.left);
+        boolean right=isBalanced(root.right);
+        return left&&right;
+    }
+    int depth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=depth(root.left);
+        int right=depth(root.right);
+        return (1+ Math.max(left,right));
+    }
+}
