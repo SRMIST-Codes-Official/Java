c26c6161ce98e538bf06ae0512cc9b6e09e0d547
Saket <saketruia18@gmail.com>
2026-09-05 23:09:13 +0530

    better solution of 3903

---
+++ Leet Code/3903. Smallest Stable Index I/better_solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int max=0;
+        int n=nums.length;
+        int[] min=new int[nums.length];
+        min[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            min[i]=Math.min(nums[i], min[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            max=Math.max(max, nums[i]);
+            if(max-min[i] <=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
